861fc5d5e33f91522d35abc7970ad0672d031ef2
nguyenduylong <duylongbka@gmail.com>
2021-06-14 20:47:54 +0900

    chapter 3 : static page

--- ror/test/controllers/users_controller_test.rb
+++ ror/test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
+      post users_url, params: { user: { email: 'duylongbka@gmail.com', name: 'duylong' } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
+    patch user_url(@user), params: { user: { email: 'duylong_updated@gmail.com', name: 'updated long' } }
     assert_redirected_to user_url(@user)
   end
 
