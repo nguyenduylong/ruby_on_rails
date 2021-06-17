11a684f2f8b0448c7dd71ab29e4bc490af7c4871
nguyenduylong <duylongbka@gmail.com>
2021-06-17 22:24:37 +0900

    nake a user model

--- ror/app/models/user.rb
+++ ror/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-    has_many :microposts, class_name: "Micropost"
-    validates :name, presence: true
-    validates :email, presence: true
+    before_save { email.downcase! }
+    validates :name, presence: true, length: { maximum:50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum:255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+    validates :password, presence: true, length: { minimum:6 }
+    has_secure_password
 end
